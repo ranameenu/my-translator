caa603ecf5b5637b6af773d1f45d61651b9e4088
meenu <meenu.rana@successive.tech>
2021-05-22 19:29:54 +0530

    translate content using API integration

--- src/App.js
+++ src/App.js
@@ -1,38 +1,127 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div class="container">
-      <div class="row ">
-        <div class="col-6 mt-3">
-          <select class="form-select p-2" aria-label="Default select example">
-            <option selected>Select Language</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-          <textarea
-            class="form-control"
-            id="exampleFormControlTextarea1"
-            rows="5"
-          ></textarea>
+class App extends Component {
+  state = {
+    language: {},
+    inputlang: '',
+    outputlang: '',
+    input: '',
+    output: '',
+  };
+
+  getLangs = async () => {
+    const res = await axios.get(
+      'https://api.cognitive.microsofttranslator.com/languages?api-version=3.0'
+    );
+
+    this.setState({ language: res.data.translation });
+  };
+
+  componentDidMount() {
+    this.getLangs();
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  getOptions = () => {
+    let options = [];
+    for (let key in this.state.language) {
+      options.push(
+        <option key={key} value={key}>
+          {this.state.language[key].name}
+        </option>
+      );
+    }
+    return options;
+  };
+
+  getTranslate = async () => {
+    var subscriptionKey = 'bbce00cf9d3c44d88c0bbea72a017304';
+    var endpoint = 'https://api.cognitive.microsofttranslator.com';
+    var location = 'centralindia';
+
+    const res = await axios({
+      baseURL: endpoint,
+      url: '/translate',
+      method: 'post',
+      headers: {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+      },
+      params: {
+        'api-version': '3.0',
+        from: this.state.inputlang,
+        to: this.state.outputlang,
+      },
+      data: [
+        {
+          text: this.state.input,
+        },
+      ],
+      responseType: 'json',
+    });
+
+    this.setState({ output: res.data[0].translations[0].text });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row ">
+          <div className="col-6 mt-5">
+            <select
+              className="form-select p-2"
+              aria-label="Default select example"
+              name="inputlang"
+              value={this.state.inputlang}
+              onChange={this.onChange}
+            >
+              {this.getOptions()}
+            </select>
+            <textarea
+              className="form-control"
+              rows="5"
+              name="input"
+              value={this.state.input}
+              onChange={this.onChange}
+            ></textarea>
+          </div>
+          <div className="col-6 mt-5">
+            <select
+              className="form-select p-2"
+              aria-label="Default select example"
+              name="outputlang"
+              value={this.state.outputlang}
+              onChange={this.onChange}
+            >
+              {this.getOptions()}
+            </select>
+            <textarea
+              className="form-control"
+              rows="5"
+              value={this.state.output}
+              // name="output"
+              disabled
+            ></textarea>
+          </div>
         </div>
-        <div class="col-6 mt-3">
-          <select class="form-select p-2" aria-label="Default select example">
-            <option selected>Select Resultant language</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-          <textarea
-            class="form-control"
-            id="exampleFormControlTextarea1"
-            rows="5"
-          ></textarea>
+        <div className="row mt-3">
+          <div className="col-3">
+            <button
+              type="button"
+              onClick={this.getTranslate}
+              className="btn btn-primary"
+            >
+              Translate
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
